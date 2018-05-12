61b36ef0229a8e05ec9cf0385f4a9e68c2945d59
Kai <kai@kais-macbook-pro.local>
2018-05-12 10:21:59 +0700

    detect volume button- lack pause button

--- HeadphoneDetection/ViewController.swift
+++ HeadphoneDetection/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import MediaPlayer
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let volumeView = MPVolumeView(frame: CGRect.zero)
+        for subview in volumeView.subviews {
+            if let button = subview as? UIButton {
+                button.setImage(nil, for: .normal)
+                button.isEnabled = false
+                button.sizeToFit()
+            }
+        }
+        UIApplication.shared.windows.first?.addSubview(volumeView)
+        UIApplication.shared.windows.first?.sendSubview(toBack: volumeView)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        AVAudioSession.sharedInstance().addObserver(self, forKeyPath: "outputVolume", options: NSKeyValueObservingOptions.new, context: nil)
+        do { try AVAudioSession.sharedInstance().setActive(true) }
+        catch {
+//            debugPrint("\(error)")
+//            print("viewWillAppear")
+        }
     }
-
-
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        AVAudioSession.sharedInstance().removeObserver(self, forKeyPath: "outputVolume")
+        do { try AVAudioSession.sharedInstance().setActive(false) }
+        catch {
+//            debugPrint("\(error)")
+//            print("ViewWillDisappear")
+        }
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        guard let key = keyPath else { return }
+        switch key {
+        case "outputVolume":
+            print("got signal")
+//            guard let dict = change, let temp = dict[NSKeyValueChangeKey.newKey] as? Float, temp != 0.5 else { return }
+////            let systemSlider = MPVolumeView().subviews.first { (aView) -> Bool in
+////                return NSStringFromClass(aView.classForCoder) == "MPVolumeSlider" ? true : false
+////                } as? UISlider
+////            systemSlider?.setValue(0.5, animated: false)
+////            guard systemSlider != nil else { return }
+//            debugPrint("Either volume button tapped.")
+//
+//            print("temp \(temp)")
+        default: print("another key")
+            break
+        }
 }
-
+}
